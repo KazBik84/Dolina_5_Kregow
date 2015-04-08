d997e18686303d5bc8c624e7fc257def9ae0ee28
kask666 <kask666@wp.pl>
2015-04-08 03:03:55 -0300

    Added tests to announcement model/controller

--- spec/models/announcement_spec.rb
+++ spec/models/announcement_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Announcement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+  	expect(build(:announcement)).to be_valid
+  end
+  it "is not valid without title" do
+  	expect(build(:announcement, title: nil)).to_not be_valid
+  end 
+  it "is not valid without message" do
+  	epect(build(:announcement, message: nil)).to_not be_valid
+  end
+  
 end
