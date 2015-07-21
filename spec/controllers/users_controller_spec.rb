81c2f540c693eb95da6472ce6f2e65bb08903bd0
kask666 <kask666@wp.pl>
2015-07-21 17:06:59 -0300

    Dodałem test tkóry sprawdza czy zmienna @user jest poprawnie asygnowana

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,16 +4,19 @@ RSpec.describe UsersController, type: :controller do
 
   describe "GET #show" do
     before(:each) do
-      user = FactoryGirl.create(:user)
+      @user = FactoryGirl.create(:user)
       #login_as(user, :scope => :user) 
-      sign_in user
-      get :show, id: user.id  
+      sign_in @user
+      get :show, id: @user.id  
     end
     it "returns http success" do
       expect(response).to have_http_status(:success)
     end
     it "render show page correctly" do
       expect(response).to render_template(:show)
+    end
+    it "assigns @user variable correctly" do
+      expect(assigns(:user)).to eq(@user)
     end
   end
 
