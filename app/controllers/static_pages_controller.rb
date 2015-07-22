ff8254b7dcd0bec658fafd83b2312f99bddb15d8
kask666 <kask666@wp.pl>
2015-07-22 15:59:01 -0300

    Dodałem link dodawania postów jeśli user to admin/wywaliłem zmienną @user z pages controllera devise 'current_user' w pełni ja zastępuje

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
     @announcements = Announcement.page(params[:page]).order('id DESC')
-    @user = current_user if user_signed_in? 
   end
 
   def contact
