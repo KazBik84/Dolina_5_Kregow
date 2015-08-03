00f6765373d7582a2aea94d9b620230644502c27
kask666 <kask666@wp.pl>
2015-08-03 16:46:34 -0300

    Dodałem validację unikalności nazwy użytkownika/naprawiłem błąd zbyt długiego pojedynczego ciągu znaków który wychodził poza ramy

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+
+  validates :username, uniqueness: true 
+  
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+           
   has_many :comments, dependent: :destroy
 end
