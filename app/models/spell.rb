2f6ab4841bd7c2b5ae275102a16456f2ae1c2275
kask666 <kask666@wp.pl>
2015-08-04 01:13:50 -0300

    Dodałem CSS-y do czarów/walidację do modelu Spell/poprawiłem html w stronach dotyczących spelli/ usunąłem formularz do wyszukiwania ze strony z wynikami spell i school

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -1,5 +1,9 @@
 class Spell < ActiveRecord::Base
 
+  validates :nazwa, :krag, :zywiol, :obszar, :zasieg, :opis, :czas, presence: true
+  validates_inclusion_of :zywiol, in: [ "Ogień", "Ziemia", "Powietrze", "Woda", "Pustka", "Maho" ]
+  validates_inclusion_of :krag, in: %w( 1 2 3 4 5 6 )
+  
   def self.find_spells(zywiol, krag, tagi)
     if tagi.blank?
       Spell.where(zywiol: zywiol, krag: krag ).to_a
