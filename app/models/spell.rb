8e2d20cce9d1b60043c41576bbef5ce3f273c0e3
kask666 <kask666@wp.pl>
2015-08-14 03:53:26 -0300

    Poprawiłem funkcję zwracającą żądane czary

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -5,26 +5,26 @@ class Spell < ActiveRecord::Base
   validates_inclusion_of :krag, in: %w( 1 2 3 4 5 6 )
   
   def self.find_spells(zywiol, krag, tagi)
-    if zywiol.blank? && krag.blank? && tagi.blank?
+    if zywiol.nil? && krag.nil? && tagi.nil?
       Spell.all
-    elsif zywiol.blank? && krag.blank? && tagi.present?
-      Spell.where('tagi @> ARRAY[?]', tagi)
-    elsif zywiol.blank? && krag.present? && tagi.blank?
+    elsif zywiol.nil? && krag.nil? && tagi.present?
+      Spell.all.where('tagi @> ARRAY[?]', tagi)
+    elsif zywiol.nil? && krag.present? && tagi.nil?
       Spell.where(krag: krag)
-    elsif zywiol.blank? && krag.present? && tagi.present?
+    elsif zywiol.nil? && krag.present? && tagi.present?
       Spell.where(krag: krag).where('tagi @> ARRAY[?]', tagi)
-    elsif zywiol.present? && krag.blank? && tagi.blank?
+    elsif zywiol.present? && krag.nil? && tagi.nil?
       Spell.where(zywiol: zywiol)
-    elsif zywiol.present? && krag.blank? && tagi.present?
+    elsif zywiol.present? && krag.nil? && tagi.present?
       Spell.where(zywiol: zywiol).where('tagi @> ARRAY[?]', tagi)
-    elsif zywiol.present? && krag.present && tagi.blank?
-      Spell.where(zywiol: zywiol, krag: krag)  
     elsif zywiol[0].blank? && krag[0].blank?
       Spell.all
     elsif zywiol[0].blank? && krag[0].present?
       Spell.where(krag: krag)
     elsif zywiol[0].present? && krag[0].blank?
       Spell.where(zywiol: zywiol)
+    elsif zywiol[0].present? && krag[0].present?
+      Spell.where(zywiol: zywiol, krag: krag)
     else
       Spell.where(zywiol: zywiol, krag: krag).where('tagi @> ARRAY[?]', tagi)
     end
