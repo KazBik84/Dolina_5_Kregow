f8df22fd2b0c05b242a1fb303836f8834438dc0c
kask666 <kask666@wp.pl>
2015-11-04 01:19:46 -0400

    Added constans for traits and spells forms

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,5 @@
+#------------------- Constans related to Clan model -------------------------------------------
+
 CLAN_NAMES = %w( Feniks Krab Smok 
                 Jednorozec Lew Modliszka 
                 Pajak Rody_Cesarskie Pomniejsze 
@@ -8,22 +10,34 @@ CLAN_NAMES_PL = [ "Feniks", "Krab", "Smok",
                 "Pająk", "Rody Cesarskie", "Pomniejsze", 
                 "Żuraw", "Skorpion", "Ronin" ]
 
-CLAN_NAMES_ARRAY = [ [ "Feniks", "Feniks" ], 
-                     [ "Krab", "Krab" ], 
-                     [ "Smok", "Smok" ], 
-                     [ "Jednorożec", "Jednorozec" ], 
-                     [ "Lew", "Lew" ], 
-                     [ "Modliszka", "Modliszka" ],
-                     [ "Pająk", "Pajak" ],
-                     [ "Rody Cesarskie", "Rody_Cesarskie"], 
-                     [ "Pomniejsze", "Pomniejsze"],
-                     [ "Żuraw", "Zuraw"], 
-                     [ "Skorpion", "Skorpion"], 
-                     [ "Ronin", "Ronin"] ]
+CLAN_NAMES_ARRAY = CLAN_NAMES_PL.zip(CLAN_NAMES)
+
+#------------------- Constans related to School model -----------------------------------------
+  #School model also use the CLAN_NAMES, CLAN_NAMES_PL and CLAN_NAMES_ARRAY constans
+
+SCHOOL_CLASSES = [ "Bushi", "Shugenja", "Mnich", "Artysta", "Ninja", "Dworzanin" ]
+
+#------------------- Constans related to Skill model -----------------------------------------
 
 SKILL_SPHARES_ARRAY = [ ["Godne", "Godna" ],
                   ["Niegodne", "Niegodna" ],
                   ["Bugei", "Bugei"], 
                   ["Kupieckie", "Kupiecka"] ]
 
-SCHOOL_CLASSES = [ "Bushi", "Shugenja", "Mnich", "Artysta", "Ninja", "Dworzanin" ]
+#------------------- Constans related to Spell model -----------------------------------------
+
+SPELL_ELEMENTS_PL = [ "Ogień", "Woda", "Powietrze", "Ziemia", "Pustka", "Maho"]
+
+SPELL_KRAG_ARRAY = [ [ "Pierszwy", "1" ], [ "Drugi", "2" ], [ "Trzeci", "3" ], 
+                     [ "Czwarty", "4" ], [ "Piąty", "5" ], [ "Szósty", "6" ] ]
+
+SPELL_TAGS_PL = [ "Rzemiosło", "Obrona", "Grom", "Pieczęć", "Bitwa", "Jadeit", "Podróż", "Iluzja", "Przepowiednie" ]
+
+#------------------- Constans related to Trait model -----------------------------------------
+TRAITS_KIND_SINGULAR = [ "Zaleta", "Wada"]
+TRAITS_KIND_PLURAL = [ "Zalety", "Wady" ]
+TRAITS_KIND_ARRAY = TRAITS_KIND_PLURAL.zip(TRAITS_KIND_SINGULAR)
+
+TRAITS_SPHARE_SINGULAR = [ "Fizyczna", "Duchowa", "Społeczna", "Materialna", "Umysłowa"]
+TRAITS_SPHARE_PLURAL = [ "Fizyczne", "Duchowe", "Społeczne", "Materialne", "Umysłowe"]
+TRAITS_SPHARE_ARRAY = TRAITS_SPHARE_PLURAL.zip(TRAITS_SPHARE_SINGULAR)
