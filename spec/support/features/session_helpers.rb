ac9118a754194172c700b315d822ae2e370f0862
kask666 <kask666@wp.pl>
2015-11-19 01:44:48 -0400

    Added 'create_character' helper

--- spec/support/features/session_helpers.rb
+++ spec/support/features/session_helpers.rb
@@ -13,13 +13,28 @@ module Features
     def sign_in
       user = create(:user)
       visit new_user_session_path  
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: user.password
+      fill_in 'Username lub Mail', with: user.username
+      fill_in 'Hasło', with: user.password
       click_button 'Log in'
     end
 
     def logout
       click_on 'Logout'
+    end
+
+    def create_comment(text)
+      fill_in 'comment_content', with: text
+      click_button ('Dodaj!')
+    end
+
+    def create_character(name, desc)
+      expect(page).to have_css('article.Feniks')
+      select 'Jednorożec', from: 'character_clan'
+      expect(page).to have_css('article.Jednorozec')
+      select 'Utaku', from: 'character_family'
+      fill_in "Imię", with: name
+      fill_in "Opis Postaci",with: desc
+      click_button "Zapisz"
     end
   end
 end
